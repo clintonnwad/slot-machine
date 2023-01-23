a98f054808c3cfe4535dde278cc96c006d057498
Clinton Nwadiukor <113151586+clintonnwad@users.noreply.github.com>
2023-01-22 22:13:41 -0500

    Header added

--- Slot Machine/GameObjects/SlotItem.swift
+++ Slot Machine/GameObjects/SlotItem.swift
@@ -4,6 +4,19 @@
 //
 //  Created by Jovi on 22/01/2023.
 //
+//  GROUP NUMBER: 20
+//  NAMES:
+//  Clinton Nwadiukor  - 301291242
+//  Jovi Chen-Mcintyre - 301125059
+//
+//  DESCRIPTION:
+//  Slot Machine application for iOS. Users can place bets, spin and either win or lose
+//
+//  REVISION HISTORY:
+//  https://github.com/clintonnwad/slot-machine/commits/main
+//
+//  DATE LAST MODIFIED:
+//  January 22, 2022
 
 import Foundation
 
